c4bb8ea7b714e1ffbb10d3b7d2295999f9bb37ff
Topacio Manrique <topacio.manrique@umvel.com>
2022-07-12 19:52:02 -0500

    feat: implement UI

--- app/src/main/java/com/example/coins/CurrencyDetailActivity.kt
+++ app/src/main/java/com/example/coins/CurrencyDetailActivity.kt
@@ -3,9 +3,9 @@ package com.example.coins
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity2 : AppCompatActivity() {
+class CurrencyDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        setContentView(R.layout.activity_currency_detail)
     }
 }
